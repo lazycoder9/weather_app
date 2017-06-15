747923e3697afce9ebf748d0044af005c8e4deec
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2017-06-16 01:21:27 +0500

    edit draw calendar function

--- lib/draw_calendar.php
+++ lib/draw_calendar.php
@@ -3,18 +3,35 @@
 namespace Lib;
 
 function draw_calendar($month, $year, $data) {
-  $headings = array('Week','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
   $month_name = date('F', mktime(0, 0, 0, $month, 10));
   echo "<h2> $month_name $year </h2>";
   echo '<table cellpadding="0" cellspacing="0" class="calendar">';
-  echo '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+  echo draw_calendar_header();
   foreach ($data as $week => $days) {
-    echo "<tr class=\"calendar-row\"><td class=\"week\">$week</td>";
+    echo '<tr class="calendar-row"><td class="week">'.$week.'</td>';
     for($i = 1; $i < 8; $i++) {
-      echo array_key_exists($i, $days) ?
-        '<td class="calendar-day '.$days[$i]['type'].'"><div class="temperature">'.$days[$i]['DAY_TEMP'].' / '.$days[$i]['NIGHT_TEMP'].'</div><div class="day-number">'.date('j',strtotime($days[$i]['day'])).'</div>'.'</td>'
-        : '<td class="calendar-day"></td>';
+      echo draw_calendar_day($days, $i);
     }
   }
   echo '</table>';
+}
+
+function draw_calendar_header() {
+  $headings = array('Week','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
+  $header_map = array_map(function($week_day) {
+    return '<td class="calendar-day-head">'.$week_day.'</td>';
+  }, $headings);
+
+  return '<tr class="calendar-row">'.implode('', $header_map).'</tr>';
+}
+
+function draw_calendar_day($daily_data, $day_number) {
+  if (array_key_exists($day_number, $daily_data)) {
+    $data_type = $daily_data[$day_number]['type'];
+    $temperature_data = $daily_data[$day_number]['DAY_TEMP'].' / '.$daily_data[$day_number]['NIGHT_TEMP'];
+    $day_number = date('j',strtotime($daily_data[$day_number]['day']));
+    return '<td class="calendar-day '.$data_type.'"><div class="temperature">'.$temperature_data.'</div><div class="day-number">'.$day_number.'</div></td>';
+  } else {
+    return '<td class="calendar-day"></td>';
+  }
 }
